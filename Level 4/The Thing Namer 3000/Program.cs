c478035bd5161adfff2771d9b2accc0cf163424c
tylerhand1 <tyha7462@colorado.edu>
2024-05-14 16:08:43 -0600

    Challenge: The Thing Namer 3000

--- Level 4/The Thing Namer 3000/Program.cs
+++ Level 4/The Thing Namer 3000/Program.cs
@@ -1,7 +1,10 @@
 ﻿Console.WriteLine("What kind of thing are we talking about?");
-string? a = Console.ReadLine();
+string? a = Console.ReadLine(); // a is the name of the thing
 Console.WriteLine("How would you describe it? Big? Azure? Tattered?");
-string? b = Console.ReadLine();
-string c = "of Doom";
+string? b = Console.ReadLine(); // b is the description of the thing
+/*
+ * c and d are string literals
+ */
+string c = "Doom";
 string d = "3000";
 Console.WriteLine("The " + b + " " + a + " of " + c + " " + d + "!");
