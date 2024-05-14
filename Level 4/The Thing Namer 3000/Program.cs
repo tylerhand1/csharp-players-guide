a39455d45696b189c33f53d4fac37ec20db7104e
tylerhand1 <tyha7462@colorado.edu>
2024-05-14 16:07:51 -0600

    Started challenge: The Thing Namer 3000

---
+++ Level 4/The Thing Namer 3000/Program.cs
@@ -0,0 +1,7 @@
+﻿Console.WriteLine("What kind of thing are we talking about?");
+string? a = Console.ReadLine();
+Console.WriteLine("How would you describe it? Big? Azure? Tattered?");
+string? b = Console.ReadLine();
+string c = "of Doom";
+string d = "3000";
+Console.WriteLine("The " + b + " " + a + " of " + c + " " + d + "!");
