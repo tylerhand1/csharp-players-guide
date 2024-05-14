361351a4f0694dd21c545a7dbf889fe6f7f56639
tylerhand1 <tyha7462@colorado.edu>
2024-05-14 16:00:21 -0600

    Challenge: The Makings of a Programmer

---
+++ Level 3/The Makings of a Programmer/Program.cs
@@ -0,0 +1,5 @@
+﻿Console.WriteLine("This is a line");
+Console.WriteLine("This is another line");
+Console.WriteLine("Yet another line");
+Console.WriteLine("A fourth one");
+Console.WriteLine("A final one");
